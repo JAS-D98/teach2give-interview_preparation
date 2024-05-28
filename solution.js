4a0bd49585e9595ff6ecd0345dd102dce0c29d7b
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-05-28 12:10:41 +0300

    solve fizzBuzz Question

---
+++ solution.js
@@ -0,0 +1,15 @@
+let fizzBuzz =(num)=>{
+        if (num%3==0 && num%5==0) {
+            return `${num} - FizzBuzz`;
+        }else if(num%3==0){
+            return `${num} - Fizz`;
+        }else if(num%5==0){
+            return `${num} - Buzz`;
+        }else{
+            return `${num}`;
+        }
+}
+
+for (let i=0; i<=100; i++){
+    console.log(fizzBuzz(i));
+}
